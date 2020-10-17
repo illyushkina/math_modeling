f9cb671101cc787d7fa0270a63dbb90f921f95ef
Алина Волотовская <71902305+illyushkina@users.noreply.github.com>
2020-10-17 12:49:16 +0200

    я молодец

---
+++ lec_2_if_else.py
@@ -0,0 +1,5 @@
+a=3
+if a>4:
+    print('hello 5')
+else:
+    print('hello a')
---
+++ lec_2_while.py
@@ -0,0 +1,12 @@
+i=5
+while i<15:
+    print(i)
+    i=i+2
+for i in 'hello vorld':
+    if i=='o':
+        break
+    print(i*2, end=' ')
+for i in 'hello world':
+    if i=='o':
+        continue
+    print(i*2, end='')
