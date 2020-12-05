32ed840575dcd1a242cc1e22d16d04bbc11b983b
Алина Волотовская <71902305+illyushkina@users.noreply.github.com>
2020-12-05 12:58:58 +0200

    я молодец

---
+++ lab_2_task_2.py
@@ -0,0 +1,10 @@
+print 
+N = int (input("введите число N: "))
+q0 = int(input("введите число q0: "))
+q1 = int(input("введите число q1 "))
+
+for i in range (0,N+1,1):
+
+    q = q0*q1
+    print(q)
+    q0 = q
---
+++ lab_3_task_3.py
@@ -0,0 +1,10 @@
+year = int(input("введите год: "))
+if year % 4==0:
+    print ("високосный")
+else:
+    print("обычный")
+    
+    
+    
+    
+    
